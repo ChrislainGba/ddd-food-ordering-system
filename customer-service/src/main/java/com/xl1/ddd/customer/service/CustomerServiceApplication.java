9407bcaec9414a2c3292ffd11439d147fc198906
Chrislain GBA <gbachrislain@gmail.com>
2024-10-30 22:40:47 +0100

    Step 2

--- customer-service/src/main/java/com/xl1/ddd/customer/service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/xl1/ddd/customer/service/CustomerServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.xl1.ddd")
-public class CustomerServiceApplication {
+public class CustomerServiceApplication implements{
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
